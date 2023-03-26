9ff8a12ed7829340aec60e9ca969ee66fab3cca2
Arjun <arjundasback515@gmail.com>
2023-03-26 20:33:45 +0600

    login component create done

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../LayOut/Main";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/login/Login";
 
 
   const router = createBrowserRouter([
@@ -11,6 +12,10 @@ import Home from "../Pages/Home/Home/Home";
         {
             path:"/",
             element: <Home></Home>
+        },
+        {
+            path:"/login",
+            element: <Login></Login>
         },
       ]
     },
